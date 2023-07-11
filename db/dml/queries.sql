72c7e19f714de8c1c0898365459f22faf8c5b1c5
Nick <65122415+damn-boi@users.noreply.github.com>
2023-07-11 11:53:52 +0300

    updated queires.sql
    Made changes to queires.sql (now actually made)

--- db/dml/queries.sql
+++ db/dml/queries.sql
@@ -1,13 +1,18 @@
 -- Определить сколько книг прочитал каждый читатель в текущем году. Вывести рейтинг чита-телей по убыванию.
 
-select count(book_id) as books_readed, reader_id FROM public.book_rental WHERE
-book_borrowed_date >= '01-01-2023' and book_returned_date < '01-01-2024' group by reader_id
-order by books_readed desc;
+SELECT reader.reader_id as "id Читателя", full_name as "Читатель", count(book_id) as "Книг прочитано"
+FROM reader
+inner JOIN book_rental on book_rental.reader_id=reader.reader_id
+where book_returned_date is not null
+and date_part('year', book_borrowed_date) = date_part('year', current_date) 
+group by "Читатель" , "id Читателя" order by "Книг прочитано" desc;
+
 
 -- Определить, сколько книг у читателей на руках на текущую дату.
 
-select count(book_id) as books_not_returned, reader_id from public.book_rental where 
-book_returned_date is null group by reader_id;
+select count(book_id) as books_not_returned
+from book_rental
+where book_returned_date is null;
 
 -- Определить читателей, у которых на руках определенная книга.
 
@@ -30,20 +35,24 @@ and book_returned_date is null;
 
 -- Книги какого издательства были самыми востребованными у читателей? Отсортируйте издательства по убыванию востребованности книг.
 
-select count(book.book_id) as books_demanded, fk_publisher_id as publisher_id from book
-inner join book_rental on book.book_id=book_rental.book_id
-group by publisher_id order by books_demanded desc;
+select count(book.book_id) as books_demanded, publisher_id, publisher_name 
+from book
+inner join book_rental using(book_id)
+inner join publisher on publisher.publisher_id = fk_publisher_id
+group by publisher_id, publisher_name order by books_demanded desc;
 
 -- Определить самого издаваемого автора.
 
-select count(book_author.book_id) as books_published, author_id from book_author inner join 
-book_rental on book_rental.book_id=book_author.book_id
-group by author_id order by books_published desc;
+select count(book_id) as "выпущено книг", author_full_name from book_author
+inner join author using(author_id)
+group by author_full_name
+order by count(book_id) desc limit 1;
 
 -- Определить среднее количество прочитанных страниц читателем за день.
 
-select avg(page_volume / minus) as days_for_reading from (select (book_returned_date-book_borrowed_date) as minus,
-page_volume
-from (select * from book_rental
-join book on book.book_id=book_rental.book_id) as g
-where book_returned_date is not null) as h;
+select full_name, (sum(page_volume) / sum(book_returned_date - book_borrowed_date)) average_pages
+from book_rental
+inner join reader using (reader_id)
+inner join book using (book_id)
+where book_returned_date is not null
+group by full_name order by average_pages desc;
